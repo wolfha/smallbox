a7014a7cb4cf4d21b3a97b13fff079eb6adfaa5d
Wolfgang Hauptfleisch <wh@augmentedlogic.com>
2023-08-28 19:19:32 +0100

    v0.2

--- src/smallbox.js
+++ src/smallbox.js
@@ -17,23 +17,20 @@ function SmallBox()
         return result;
     }
 
-
     this.add = function(content_id, open_class){
-
 
         var sbrand = this.makeid(5);
         var sb = "sb-" + sbrand;
         var body = document.querySelector("body");
         document.querySelector(content_id).style.display = "none";
 
-        var overlay_html = "<div id='smallbox-overlay-id' class='smallbox-overlay'></div>";
-        var modal_html = "<div id='"+ sb +"' class='smallbox-container'>" +
+        var modal_html = "<div id='"+ sb +"' class='smallbox-container smallbox-overlay'><div class='smallbox-display'>" +
             "<div class='smallbox-close-container'><div class='smallbox-close-button smallbox-close-"+ sbrand +"'></div></div>" +
-            "<div id='sb-inner-" + sbrand + "' class='smallbox-inner'></div></div>";
+            "<div id='sb-inner-" + sbrand + "' class='smallbox-inner'></div></div></div>";
 
         // appending the modal div with content
         var child = document.createElement("div");
-        child.innerHTML = overlay_html + modal_html;
+        child.innerHTML = modal_html;
         body.appendChild(child);
         var modal_obj = document.querySelector("#" + sb);
 
@@ -43,26 +40,18 @@ function SmallBox()
         //
         //document.querySelector(open_class).addEventListener("click", (e) => {
         document.querySelector(open_class).addEventListener("click",function(e) {
-                modal_obj.style.display = "block";
+                modal_obj.style.display = "flex";
 
                 var content_div = document.getElementById(content_id.replace("#", ""));
                 var sb_inner = document.getElementById("sb-inner-" + sbrand);
                 sb_inner.innerHTML = content_div.innerHTML;
 
-
                 var h = modal_obj.offsetHeight;
                 var w = modal_obj.offsetWidth;
-
-                //console.log("vanilla: " + h + " " + w);
 
                 //need to check
                 body.classList.add("smallbox-stop-scrolling");
                 body.addEventListener("touchmove", function(e){e.preventDefault()});
-                document.querySelector("#smallbox-overlay-id").style.display = "block";
-
-                // BUG QUESTIOn DIFFERENCE TO JQUERY
-                modal_obj.style.marginTop = "-" +  h/2 +"px";
-                modal_obj.style.marginLeft = "-" +  w/2 +"px";
         });
 
         //
@@ -70,7 +59,7 @@ function SmallBox()
         //
         document.querySelector(".smallbox-close-" + sbrand).addEventListener("click", function(e) {
                 modal_obj.style.display = "none";
-                document.querySelector("#smallbox-overlay-id").style.display = "none";
+                document.querySelector(".smallbox-container").style.display = "none";
 
                 // need to check that
                 body.removeEventListener("touchmove", function(e){e.preventDefault()});
@@ -78,17 +67,14 @@ function SmallBox()
         });
 
 
-        //
-        //
-        //
-        document.querySelector("#smallbox-overlay-id").addEventListener("click", function(e) {
-                modal_obj.style.display = "none";
-                document.querySelector("#smallbox-overlay-id").style.display = "none";
-
-                // need to check that
-                body.removeEventListener("touchmove", function(e){e.preventDefault()});
-                body.classList.remove("smallbox-stop-scrolling");
-        });
+       document.querySelector("#" + sb).addEventListener("click", function(e) {
+                              if (e.composedPath()[0] === this) {
+                                //console.log("clicked " + sb);
+                                document.querySelector("#" + sb).style.display = "none";
+                                body.removeEventListener("touchmove", function(e){e.preventDefault()});
+                                body.classList.remove("smallbox-stop-scrolling");
+                              }
+       });
 
     }
 
